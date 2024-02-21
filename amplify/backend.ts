db1cd20c9316bc2d27c4f003260ffcccb6d4011d
Erica Eaton <eatoeric@amazon.com>
2024-02-21 09:15:59 -0800

    add storage

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,16 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
+import { storage } from './storage/resource.js';
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
+  storage
 });
+
+const authRole = backend.auth.resources.authenticatedUserIamRole;
+backend.storage.resources.bucket.grantReadWrite(authRole);
+
+const unauthRole = backend.auth.resources.unauthenticatedUserIamRole;
+backend.storage.resources.bucket.grantRead(unauthRole);
